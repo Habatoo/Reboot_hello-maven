5bf36250fa17f75eb7e5dc294c726ce04c700bf1
habatoo <habatoo@gmail.com>
2021-08-06 17:47:32 +0700

    new master maven feature

--- src/main/java/sber/reboot/Main.java
+++ src/main/java/sber/reboot/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println(StringUtils.capitalize("hello maven"));
+                System.out.println(StringUtils.capitalize("hello master maven"));
 		System.out.println(StringUtils.upperCase("Hello Maven"));
 	}
 }
