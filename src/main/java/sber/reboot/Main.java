d701c2e2097b4ef6ea53fdf817f628add83f160a
habatoo <habatoo@gmail.com>
2021-08-07 23:27:14 +0700

    edit java space border

--- src/main/java/sber/reboot/Main.java
+++ src/main/java/sber/reboot/Main.java
@@ -3,11 +3,10 @@ package sber.reboot;
 import org.apache.commons.lang3.StringUtils;
 
 public class Main {
-
-	public static void main(String[] args) {
-		System.out.println(StringUtils.capitalize("hello maven"));
-                System.out.println(StringUtils.capitalize("hello master maven"));
-		System.out.println(StringUtils.upperCase("Hello Maven"));
-                System.out.println(StringUtils.upperCase("Hello new Maven feature"));
-	}
+    public static void main(String[] args) {
+        System.out.println(StringUtils.capitalize("hello maven"));
+        System.out.println(StringUtils.capitalize("hello master maven"));
+        System.out.println(StringUtils.upperCase("Hello Maven"));
+        System.out.println(StringUtils.upperCase("Hello new Maven feature"));
+    }
 }
